d339152e86701a5725aeddafb7aafda78b92c59a
Gustavo Lima <gustavolimamendes2@gmail.com>
2024-04-30 12:24:41 -0300

    rota para transferir

--- src/controllers/controlador.js
+++ src/controllers/controlador.js
@@ -128,7 +128,7 @@ const sacar = (req, res) => {
     if(conta.usuario.saldo < valor){
         return res.json({mensagem: "Saldo insuficiente!"});
     } else {
-        conta.usuario.saldo -= valor;
+        conta.saldo -= valor;
     }
 
     return res.send()
@@ -136,7 +136,30 @@ const sacar = (req, res) => {
 }
 
 const transferir = (req, res) => {
-    
+    const {numero_conta_origem, numero_conta_destino, valor, senha} = req.params;
+
+    const conta_origem = contas.find(conta => conta.numero === numero_conta_origem);
+    const conta_destino = contas.find(conta => conta.numero === numero_conta_destino);
+    if(!conta_origem || !conta_destino) {
+        return res.json({mensagem: "Conta inexistente!"});
+    }
+
+    if(conta_origem.usuario.senha !== senha) {
+        return res.json({mensagem: "Senha incorreta!"});
+    }
+
+    conta_origem.saldo -= valor;
+    conta_destino.saldo += valor;
+
+    let registro = {
+        data: new Date(),
+        numero_conta_origem: numero_conta_origem,
+        numero_conta_destino: numero_conta_destino,
+        valor: valor
+    }
+
+    bancoDeDados.transferencias.push(registro);
+    return res.send()
 }
 
 
